d12d8b42911440b2366ca94a3090e7a5c1f49c21
Oscar Jaimes <oscar@jaimes.io>
2023-05-12 21:13:34 -0600

    Basic API Functions And Tests (#2)

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
-export function hello() {
-  return "hello world";
+const axios = require('axios');
+const http = axios.create({
+  baseURL: 'https://open.alberta.ca/api/3'
+});
+
+
+const getAllDatasets = async () => {
+  try {
+    const res = await http.get('/action/package_list');
+    return res.data;
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+};
+
+const getAllTagNames = async () => {
+  try {
+    const res = await http.get('/action/tag_list');
+    return res.data;
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+};
+
+module.exports = {
+  getAllDatasets,
+  getAllTagNames
 }
